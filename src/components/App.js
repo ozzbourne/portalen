d8103cde77bc7b0c827a068f556f94c53fad93ab
Oskar Brandt <ogrilus@Oskars-MacBook-Pro.local>
2019-02-26 20:44:03 +0100

    Test if works in production

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ class App extends Component {
   authHandler = async (authData) => {
     const userApp = await base.fetch(this.props.match.params.appId, {context: this});
     // this.props.updateOwner(userApp.owner);
-    alert(userApp.owner);
+    alert(authData.user.email);
     this.setState({isLoading: false})
   }
   
